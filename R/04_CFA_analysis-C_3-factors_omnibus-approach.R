0f4c10b3119f2cb119a91cae5d5f62411959f3ec
Kelvin Gorospe <kdgorospe@gmail.com>
2022-10-08 14:33:24 -0400

    output measurement invariance model fit indices as a table and fix t-test section

--- R/04_CFA_analysis-C_3-factors_omnibus-approach.R
+++ R/04_CFA_analysis-C_3-factors_omnibus-approach.R
@@ -7,7 +7,7 @@ library(tidyverse)
 library(googledrive)
 
 # Restarting point
-load("2022-09-05_all-data-prior-to-CFA_pooled-qs-removed_likert-standardized_NAs-dropped.RData") 
+load("2022-10-08_all-data-prior-to-CFA_pooled-qs-removed_likert-standardized_NAs-dropped.RData") 
 
 ######################################################################################################
 # QUESTION FOR LISA: CONFIRM THAT FIRST STEP IS TO IDENTIFY AN EXPLORATORY MODEL USING EFA and confirm on the POST data
@@ -15,25 +15,20 @@ load("2022-09-05_all-data-prior-to-CFA_pooled-qs-removed_likert-standardized_NAs
 
 # Define model:
 # Only allow one item to load onto each latent variable (assumption with CFA is that each items loads equally)
-# Additional guidance: try testing an "expected" model based on quesitonnaire (e.g., "understanding," "skills," etc) and use anova to compare with EFA-suggested model
 # Use EFA (data) and expected model (theory) along with model fit measures and anova to arrive at final model
 
-# REMINDER: three factor model has really bad RMSEA (> 0.1); borderline acceptable cutoff would be < 0.08
-# EFA Results for THREE factors: https://docs.google.com/spreadsheets/d/1kZZ86hJYzD80pWJzTSI2UVlvumgmdh0exPSwoTb8lb0/edit#gid=1196504491
-# model <- '
-# process =~ skills_developH0 + skills_evalH0 + skills_testH0 + skills_withothers + understanding_relatetolife + understanding_sciprocess
-# identity =~ attitudes_career + attitudes_discussing + attitudes_enthusiastic + attitudes_workwithothers + integration_applyingknowledge
-# confidence =~ attitudes_confidentunderstanding + understanding_ecology + understanding_fertilization
-#  '
-
-
-# EFA Results for FOUR factors: https://docs.google.com/spreadsheets/d/1y5w1OhuB2VbnYgcQ5W9d5iI8qeC2xQKIQVf-LetNMmc/edit#gid=1486559735
+# EFA Results for FOUR factors: https://docs.google.com/spreadsheets/d/1PV5B4tzEFQUa1_8KEfHZAFZoYlGPVUG-rbk7Wqgf6b0/edit#gid=1486559735
 model <- '
 process =~ skills_developH0 + skills_evalH0 + skills_testH0
-identity =~ attitudes_career + attitudes_discussing + attitudes_enthusiastic 
+interest =~ attitudes_career + attitudes_discussing + attitudes_enthusiastic 
 integration =~ integration_applyingknowledge + integration_connectingknowledge
 content =~ understanding_ecology + understanding_fertilization
  '
+
+# NOTE: See FOUR factor results with more permissive cutoff = 0.4: https://docs.google.com/spreadsheets/d/1y5w1OhuB2VbnYgcQ5W9d5iI8qeC2xQKIQVf-LetNMmc/edit#gid=1486559735 
+# Only gain one more variable and not in any of the factors that could use more measured variables
+# Lowering cutoff further to 0.3 makes it more difficult to interpret factors (e.g., work effectively with others starts to load with scientific process factor)
+# Since lowering the cutoff doesn't gain us much, keep stricter 0.7 cutoff so that we can say we chose a model with strong loadings
 
 # Fit indices (results) are the same for parameterization = "delta" (same setting as measurement invariance) or "theta"
 # Use MLR estimator for continuous-ish variables (e.g., often used when there are 5 or more response options)
@@ -118,19 +113,91 @@ test_mod_fit[1,] <- round(data.matrix(fitmeasures(test_mod, fit.measures = c("ch
 
 test_mod_fit
 
+# Compare model fits:
+no_groups_fit
+test_mod_fit
 
+# Your CFA is a pretty good fit for "real life" data and 
+# yes your no groups model is a better fit which suggests that you have measurement invariance. 
+
+# Following: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5145197/
+# Reproduce Table 3 for these results, modified for omnibus approach
+
+# Get Chi Sq and (DF)
+chisq_base <- round(fitmeasures(no_groups, fit.measures = c("chisq.scaled")), digits = 3)
+df_base <- fitmeasures(no_groups, fit.measures = c("df.scaled"))
+chisq_df_base <- paste(chisq_base,
+                       " (", 
+                       df_base,
+                       ")",
+                       sep = "", collapse = "")
+chisq_test <- round(fitmeasures(test_mod, fit.measures = c("chisq.scaled")), digits = 3)
+df_test <- fitmeasures(test_mod, fit.measures = c("df.scaled"))
+chisq_df_test <- paste(chisq_test,
+                       " (", 
+                       df_test,
+                       ")",
+                       sep = "", collapse = "")
+# Get P-value
+p_base <- round(fitmeasures(no_groups, fit.measures = c("pvalue.scaled")), digits = 3)
+p_test <- round(fitmeasures(test_mod, fit.measures = c("pvalue.scaled")), digits = 3)
+
+# Get CFI
+cfi_base <- round(fitmeasures(no_groups, fit.measures = c("cfi.scaled")), digits = 3)
+cfi_test <- round(fitmeasures(no_groups, fit.measures = c("cfi.scaled")), digits = 3)
+
+# GEt RMSEA
+rmsea_base <- round(fitmeasures(no_groups, fit.measures = c("cfi.scaled")), digits = 3)
+rmsea_ci_base <- paste(round(rmsea_base, digits = 3),
+                       " (", 
+                       round(fitmeasures(no_groups, fit.measures = c("rmsea.ci.lower.scaled")), digits = 3),
+                       "-",
+                       round(fitmeasures(no_groups, fit.measures = c("rmsea.ci.upper.scaled")), digits = 3),
+                       ")",
+                       sep = "", collapse = "")
+
+rmsea_test <- round(fitmeasures(test_mod, fit.measures = c("cfi.scaled")), digits = 3)
+rmsea_ci_test <- paste(round(rmsea_test, digits = 3),
+                       " (", 
+                       round(fitmeasures(test_mod, fit.measures = c("rmsea.ci.lower.scaled")), digits = 3),
+                       "-",
+                       round(fitmeasures(test_mod, fit.measures = c("rmsea.ci.upper.scaled")), digits = 3),
+                       ")",
+                       sep = "", collapse = "")
+
+# Get SRMR
+srmr_base <- round(fitmeasures(no_groups, fit.measures = c("srmr")), digits = 3)
+srmr_test <- round(fitmeasures(test_mod, fit.measures = c("srmr")), digits = 3)
+
+# Get deltas
+# FIX IT: Revisit abs(); Look up model rejection cutoffs for deltas (test - base vs base - test)
+delta_chisq <- round(abs(chisq_base - chisq_test), digits = 3)
+delta_df <- abs(df_base - df_test)
+delta_chisq_df <- paste(delta_chisq, " (", delta_df, ")", sep = "", collapse = "")
+delta_cfi <- abs(cfi_base - cfi_test)
+delta_rmsea <- abs(rmsea_base - rmsea_test)
+delta_srmr <- abs(srmr_base - srmr_test)
+
+# Assemble into table:
+mi_table <- matrix(NA, nrow = 2, ncol = 9)
+colnames(mi_table) <- c("chisq_df", "pvalue_scaled", "cfi_scaled", "rmsea_ci", "srmr", "delta_chisq_df", "delta_cfi", "delta_rmsea", "delta_srmr")
+rownames(mi_table) <- c("no_groups", "with_groups")
+mi_table[1,] <- c(chisq_df_base, round(c(p_base, cfi_base), digits = 3), rmsea_ci_base, srmr_base, NaN, NaN, NaN, NaN)
+mi_table[2,] <- c(chisq_df_test, p_test, cfi_test, rmsea_ci_test, srmr_test, delta_chisq_df, delta_cfi, delta_rmsea, delta_srmr)
+
+mi_file <- "mi_omnibus_approach.csv"
+write.csv(mi_table, quote = FALSE, row.names = TRUE, file = mi_file)
+# drive_upload(mi_file, path = as_dribble("REMS_SALG/Results")) # for initial upload
+drive_update(file = as_id("1zlARe49uQg3o_73bagfusaX4vkdojMLQ"), media = mi_file)
+file.remove(mi_file)
 ######################################################################################################
 # STEP 4 - Compare the null and pre/post models
 
-# CONFIRM WITH LISA: emailed on 9/5/22
-test_model_fit
-no_groups_fit
-
 anova(no_groups, test_mod)
+# Output / report result?
+
 ######################################################################################################
 # STEP 5 - Calculate means in SALG responses for the three latent factors pre vs post and do T-test for significance
-
-# QUESTION FOR LISA: How to calculate means for the three latent factors and compare - is it just a T-test for statistical significance?
 
 # FIX IT - OUTPUT FINAL SAMPLE NUMBERS:
 tidy_dat_all %>%
@@ -163,43 +230,33 @@ group_means <- tidy_dat_all %>%
 # CALCULATE MEANS FOR LATENT FACTORS:
 group_means %>%
   rowwise() %>%
-  mutate(process = mean(skills_developH0, skills_evalH0, skills_testH0, skills_withothers, understanding_relatetolife, understanding_sciprocess),
-         identity = mean(attitudes_career, attitudes_discussing, attitudes_enthusiastic, attitudes_workwithothers, integration_applyingknowledge),
-         confidence = mean(attitudes_confidentunderstanding, understanding_ecology, understanding_fertilization)) %>%
-  select(test, process, identity, confidence)
+  mutate(process = mean(skills_developH0, skills_evalH0, skills_testH0),
+         interest = mean(attitudes_career, attitudes_discussing, attitudes_enthusiastic),
+         integration = mean(integration_applyingknowledge, integration_connectingknowledge),
+         content = mean(understanding_ecology, understanding_fertilization)) %>%
+  select(test, process, interest, integration, content)
 
+# NOTE: this collapses all variables for a shared factor into a single vector; one for pre and one for post data and performs a t-test on the two vectors
+# i.e. - individual variables are lost in that the t-test is not taking a mean of the means for each of the three variables in a factor, rather a single mean for all the data points
 # T-Test for PROCESS
-t.test(x = tidy_dat_all %>% filter(test == "pre") %>% select(-c("Number", "year", "test")) %>% select(skills_developH0, skills_evalH0, skills_testH0, skills_withothers, understanding_relatetolife, understanding_sciprocess),
-       y = tidy_dat_all %>% filter(test == "post") %>% select(-c("Number", "year", "test")) %>% select(skills_developH0, skills_evalH0, skills_testH0, skills_withothers, understanding_relatetolife, understanding_sciprocess))
+t.test(x = tidy_dat_all %>% filter(test == "pre") %>% select(-c("Number", "year", "test")) %>% select(skills_developH0, skills_evalH0, skills_testH0),
+       y = tidy_dat_all %>% filter(test == "post") %>% select(-c("Number", "year", "test")) %>% select(skills_developH0, skills_evalH0, skills_testH0))
 
+# T-test for INTEREST
+t_interest <- t.test(x = tidy_dat_all %>% filter(test == "pre") %>% select(-c("Number", "year", "test")) %>% select(attitudes_career, attitudes_discussing, attitudes_enthusiastic),
+       y = tidy_dat_all %>% filter(test == "post") %>% select(-c("Number", "year", "test")) %>% select(attitudes_career, attitudes_discussing, attitudes_enthusiastic))
 
-# T-test for IDENTITY
-t.test(x = tidy_dat_all %>% filter(test == "pre") %>% select(-c("Number", "year", "test")) %>% select(attitudes_career, attitudes_discussing, attitudes_enthusiastic, attitudes_workwithothers, integration_applyingknowledge),
-       y = tidy_dat_all %>% filter(test == "post") %>% select(-c("Number", "year", "test")) %>% select(attitudes_career, attitudes_discussing, attitudes_enthusiastic, attitudes_workwithothers, integration_applyingknowledge))
+# T-test for INTEGRATION
+t.test(x = tidy_dat_all %>% filter(test == "pre") %>% select(-c("Number", "year", "test")) %>% select(integration_applyingknowledge, integration_connectingknowledge),
+       y = tidy_dat_all %>% filter(test == "post") %>% select(-c("Number", "year", "test")) %>% select(integration_applyingknowledge, integration_connectingknowledge))
 
-# T-test for CONFIDENCE
-t.test(x = tidy_dat_all %>% filter(test == "pre") %>% select(-c("Number", "year", "test")) %>% select(attitudes_confidentunderstanding, understanding_ecology, understanding_fertilization),
-       y = tidy_dat_all %>% filter(test == "post") %>% select(-c("Number", "year", "test")) %>% select(attitudes_confidentunderstanding, understanding_ecology, understanding_fertilization))
+# T-test for CONTENT
+t.test(x = tidy_dat_all %>% filter(test == "pre") %>% select(-c("Number", "year", "test")) %>% select(understanding_ecology, understanding_fertilization),
+       y = tidy_dat_all %>% filter(test == "post") %>% select(-c("Number", "year", "test")) %>% select(understanding_ecology, understanding_fertilization))
 
-
-
-# QUESTION FOR LISA: Not part of our original research question, but if we want to test: do different genders respond the same way to the intervention?
-
-# QUESTION FOR LISA: What about this type of code?
-# OLD CODE:
-# To constrain all the latent means (the omnibus test), you can just add "means" to the vector of parameters to constrain in the group.equal argument.  
-compare <- measEq.syntax(configural.model = model,
-                         data = tidy_dat_all,
-                         ordered = FALSE, # ie all variables are ordinal
-                         parameterization = "delta", # recommended by Svetina et al for baseline model specification of ordinal variables
-                         ID.cat = "Wu.Estabrook.2016", # method for identifying residual variance for ordinal variables [this is also the DEFAULT setting]
-                         ID.fac = "std.lv", # std.lv = standardize latent variables to have a mean of 0 and a variance of 1 (can now interpret these as CORRELATIONS)
-                         group = "test", # column name defining groups
-                         group.equal = c("loadings", "intercepts", "means"))
-
-model.compare <- as.character(compare)
-fit.compare <- cfa(model.compare, data = tidy_dat_all, group = "test", ordered = FALSE, meanstructure = TRUE)
-summary(fit.compare) 
-
-
-
+# All significant differences between pre and post
+# Students were increased their:
+# perceived understanding of the scientific process (PROCESS)
+# interest in marine science (INTEREST)
+# their perceived ability to connect what they learn in classes to other areas
+# their perceived knowledge of scientific content (CONTENT)
